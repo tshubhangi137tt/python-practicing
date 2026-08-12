098889cb9cde97ef993c1fd2dc5c1728155a6175
unknown <you@example.com>
2026-08-12 23:44:37 +0530

    initialising commit

---
+++ comments and escapes.py
@@ -0,0 +1,13 @@
+print('hello \'world\' ')
+print("hello \nworld")
+print("hello \t world")
+print("hello \\ world") 
+# \\ is used for backslash 
+print("hello \\\\ world")
+# \\\\ this is used for double backslash
+# ctrl + forwardslash is used to comment any line 
+print("hello \\\'world")
+# \\\' is used for \' backslash singlequote
+print(r"hello \n world")
+# this r is  shortcut it treat escape char as normal char
+
