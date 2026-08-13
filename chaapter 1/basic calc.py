575ad503cf1dfa0d43e56b0061e9ae82b15e534a
unknown <you@example.com>
2026-08-13 23:39:35 +0530

    initial commit

---
+++ chaapter 1/basic calc.py
@@ -0,0 +1,10 @@
+print(2+4*3)
+print(2/4) #floating point division
+print(4/2) 
+print(4//2) #integer divivsion
+print(2//4)
+print(2**3)#power
+print(2**0.5)
+print(round(2**0.5,4))# it will display only 4 digits after decimal
+print(2*3+(3+5)/3*5//7%3) #in this associativity and precedence order matters
+print(3%2) # used for reminder
